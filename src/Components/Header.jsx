1f5ed278851b5711d8324f42fe066ebb66c4d82e
Alan Stall <alan_kstall@hotmail.com>
2022-10-18 10:02:04 -0300

    header fixed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ImHome3 } from 'react-icons/im';
+import { AiOutlineMenuUnfold } from 'react-icons/ai';
 
 export function Header() {
   const navigate = useNavigate();
@@ -10,6 +11,7 @@ export function Header() {
       <div className="grid grid-flow-col bg-blue-900 p-1 mx-1 my-3 sm:mx-3 rounded-lg">
         
         <div className='flex justify-start gap-1'>
+          
           <div className="card w-[65px] h-[50px]">
             <a            
               className="btn btn-ghost fill-[#ffffff]" onClick={() => navigate('/')}
@@ -21,13 +23,10 @@ export function Header() {
           </div>
 
           <div className="">
-
             <div className="dropdown dropdown-right">
-
-              <label tabIndex={0} className="btn btn-ghost p-0.5 w-10">
-                <svg className='pr-1' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+              <label tabIndex={0}>
+                <AiOutlineMenuUnfold className="btn btn-ghost p-0.5 w-12 fill-[#ffffff] rounded-xl" size={'30px'} x="0px" y="0px"/>
               </label>
-
               <ul tabIndex={0} className="menu menu-compact dropdown-content bg-blue-600 rounded-box">
                 <li><button className='btn btn-ghost btn-xs normal-case content-center py-6 text-[7px] w-[120px] xs:text-[10px] xs:w-[150px] sm:py-4 sm:text-[10px] sm:w-[300px]' onClick={() => navigate('/Add')}>ADICIONAR</button></li>
                 <li><button className='btn btn-ghost btn-xs normal-case content-center py-6 text-[7px] w-[120px] xs:text-[10px] xs:w-[150px] sm:py-4 sm:text-[10px] sm:w-[300px]' onClick={() => navigate('/ListRecords')}>MINHA LISTA</button></li>
@@ -35,7 +34,6 @@ export function Header() {
                 <li><button className='btn btn-ghost btn-xs normal-case content-center py-6 text-[7px] w-[120px] xs:text-[10px] xs:w-[150px] sm:py-4 sm:text-[10px] sm:w-[300px]' onClick={() => navigate('/StudyNotRandom')}>ESTUDAR PELA ORDEM DE CADASTRO</button></li>                
               </ul>
             </div>
-
           </div>
 
         </div>
