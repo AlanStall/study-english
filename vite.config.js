21aaec61c3be75efb373211ccf5c962a45ee8b7d
Alan Stall <alan_kstall@hotmail.com>
2022-10-25 11:38:32 -0300

    fixed url error?

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'
+/* import path from 'path' */
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        app: './main.html',
+      },
+    },    
+  },
+ 
+  server: {
+    open: '/main.html',
+  },
   
-  devServer: {
+  /* devServer: {
     contentBase: path.join(__dirname, "public"),
     watchContentBase: true,
     publicPath: "/",
     historyApiFallback: true
-}
+} */
 });
 
 
