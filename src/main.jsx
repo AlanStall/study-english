299ea5089720315c48af9b301ff29473542b4422
Alan Stall <alan_kstall@hotmail.com>
2022-10-25 20:24:29 -0300

    update vercel.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Routes from './Routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <Routes />
-    </Router>
+      <Outlet/>
+    </HashRouter>
   </React.StrictMode>
 );
