c5250797ff8376b7749d147081b95bc0eacf739d
Alan Stall <alan_kstall@hotmail.com>
2022-10-25 17:07:30 -0300

    routes vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-/* import path from 'path' */
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  build: {
-    rollupOptions: {
-      input: {
-        app: './index.html',
-      },
-    },    
-  },
- 
-  server: {
-    open: '/index.html',
-  },
-  
-  /* devServer: {
-    contentBase: path.join(__dirname, "public"),
-    watchContentBase: true,
-    publicPath: "/",
-    historyApiFallback: true
-} */
 });
 
 
