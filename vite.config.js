bdf714d08dd14f38bfbbbafdf2548fa2fe400be6
Alan Stall <alan_kstall@hotmail.com>
2022-10-25 10:45:12 -0300

    url-refresh-error-try-solve

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()], 
+  
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    watchContentBase: true,
+    publicPath: "/dist/",
+    historyApiFallback: true
+}
 });
+
+
+
